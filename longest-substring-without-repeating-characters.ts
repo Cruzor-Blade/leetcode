194553cc2a7b2ed936ed9d35e5f359c8b18eb9ae
= <cruzorbladex@gmail.com>
2024-08-14 22:48:10 +0100

    Iterated on the Longest-substring-without-repeating-characters leetcode problem solution to improve performance

--- longest-substring-without-repeating-characters.ts
+++ longest-substring-without-repeating-characters.ts
@@ -2,15 +2,17 @@ function lengthOfLongestSubstring(s: string): number {
   let length = 0;
   const set = new Set();
 
-  for (const charIndex in [...s]) {
-    for (const subChar of s.slice(Number(charIndex))) {
-      if (set.has(subChar)) {
-        set.clear();
-        break;
-      } else {
-        set.add(subChar);
-      }
-      if (set.size > length) length = set.size;
+  for (let charIndex=0; charIndex<s.length; charIndex++) {
+    const subCharLength = s.length-charIndex;
+    for (let subCharIndex=0; subCharIndex<subCharLength; subCharIndex++) {
+        const subChar = s[charIndex+subCharIndex];
+        if(set.has(subChar)) {
+            set.clear();
+            break;
+        } else {
+            set.add(subChar);
+            length = Math.max(set.size, length);
+        }
     }
   }
   return length;
