e68cc708bdb19e5888265e388d232641c2ed710b
= <cruzorbladex@gmail.com>
2024-08-26 12:02:23 +0100

    Implemented another longest-palindromic-substring problem solution

--- longest-palindromic-substring.ts
+++ longest-palindromic-substring.ts
@@ -27,6 +27,35 @@ function longestPalindrome(s: string): string {
     return longest;
 }
 
+
+function longestPalindrome2(s: string): string {
+    let longest = s[0]; // s has a length of at least one
+    
+    //sliding window from indice `left` to indice `right`
+    for (let left = 0; left+longest.length <s.length; left++) {
+        for (let right = left+longest.length+1; right<=s.length; right++ ) {
+            let ssl=Math.floor((left+right)/2), ssr=Math.ceil((left+right)/2);
+            let ssIsPalindrome=true;
+
+            // check if the substring from `ssl` to `ssr` indexes if it is a palindrome
+            while (ssl>=left && ssr<=right) {
+                if(s[ssl] != s[ssr-1]) {
+                    ssIsPalindrome=false;
+                    break;
+                };
+                ssl--, ssr++;
+            };
+
+            if(ssIsPalindrome) {
+                // the inner `for` loop garantees that the current substring is longer than `longest`
+                longest = s.slice(left, right);
+            }
+        };
+    };
+
+    return longest;
+};
+
 // Test cases
-console.assert(longestPalindrome("babad") === "bab" || longestPalindrome("babad") === "aba", "Test case 1 failed");
-console.assert(longestPalindrome("cbbd") === "bb", "Test case 2 failed");
+console.assert(longestPalindrome2("babad") === "bab" || longestPalindrome("babad") === "aba", "Test case 1 failed");
+console.assert(longestPalindrome2("cbbd") === "bb", "Test case 2 failed");
