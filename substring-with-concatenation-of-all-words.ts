db9f1bf339cb4321b809a257a699d6d429b04b5f
= <cruzorbladex@gmail.com>
2024-08-30 20:43:19 +0100

    Slightly different implementation for the substring-with-concatenation-of-all-words problem solution

--- substring-with-concatenation-of-all-words.ts
+++ substring-with-concatenation-of-all-words.ts
@@ -1,36 +1,28 @@
 function findSubstring(s: string, words: string[]): number[] {
+    words.sort()
     const wlen = words[0].length;
     const winSpan = words.length*wlen;
     const indices:number[] = [];
     let matches:number;
-    let currentMap:{ [key: string]: number }, word:string;
-
-    let wordsMap: { [key: string]: number } = {};
-    for (let word of words) {
-        wordsMap[word] = (wordsMap[word] || 0) + 1;
-    }
+    let subs:string[] = [];
 
     for (let left = 0; left <=s.length-winSpan; left++) {
         matches = 0;
-        currentMap ={};
+        subs.length=0;
 
         for (let i=0; i<words.length; i++) {
-            word = s.substring(left+i*wlen, left+(i+1)*wlen);
-            if(wordsMap[word]===undefined) break;
+            subs.push(s.substring(left+i*wlen, left+(i+1)*wlen));
+        };
 
-            currentMap[word] = (currentMap[word]||0)+1;
-            if(currentMap[word]>wordsMap[word]) break;
-            matches +=1;
+        subs.sort();
+
+        for (let i=0; i<words.length; i++) {
+            if(words[i] !== subs[i]) break;
+            matches+=1;
         };
 
         if(matches===words.length) indices.push(left);
     };
 
-
     return indices;
 };
-
-
-console.assert(findSubstring("barfoothefoobarman", ["foo", "bar"]).every(value => [0, 9].includes(value)));
-console.assert(findSubstring("wordgoodgoodgoodbestword", ["word","good","best","word"]).length===0);
-console.assert(findSubstring("barfoofoobarthefoobarman", ["bar","foo","the"]).every(value => [6,9,12].includes(value)));
