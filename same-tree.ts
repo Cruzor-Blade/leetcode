6ea9b1f5ce34d538320f4fe59d7e117e8eb74f93
= <cruzorbladex@gmail.com>
2024-09-09 21:32:40 +0100

    Solved the same-tree problem

---
+++ same-tree.ts
@@ -0,0 +1,13 @@
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+};
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if(p === null && q === null) return true;
+    if(p === null || q === null) return false;
+
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
